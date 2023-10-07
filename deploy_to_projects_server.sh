57975d1b7356edf3fcc62995dc6505e6f25f263a
Wellington Barros Moraes <wellingtonbmoraes@hotmail.com>
2023-10-07 11:48:50 -0300

    feat: Added the world to project due to backup

--- deploy_to_projects_server.sh
+++ deploy_to_projects_server.sh
@@ -3,5 +3,5 @@
 PROJECTS_MACHINE=ubuntu@168.75.66.176
 SSH_PUB_FILE=~/.ssh/projects.key.pub
 
-scp -r -i ${SSH_PUB_FILE} * ${PROJECTS_MACHINE}:~/drt-minecraft-server
+scp -r -i ${SSH_PUB_FILE} * ${PROJECTS_MACHINE}:~/drt-minecraft-server/
 ssh -o StrictHostKeyChecking=no -i ${SSH_PUB_FILE} ${PROJECTS_MACHINE} "cd /home/ubuntu/drt-minecraft-server && bash run_server.sh"
